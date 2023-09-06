cf93b7c58bab1d6193d85079397211e8d919a849
TheSadFreedom <nikita.kun.1.6@gmail.com>
2023-09-06 05:56:26 -0700

    Поменял название

--- src/Pages/MainPage/MainPages.jsx
+++ src/Pages/MainPage/MainPages.jsx
@@ -1,21 +1,10 @@
 //NPM
 import React from "react";
-import { useMediaQuery } from "@mui/material";
 //Styles
-import {
-  MainPageStyle,
-  UTPSlide
-} from "./MainPages.styled";
+import { MainPageStyle, UTPSlide } from "./MainPages.styled";
 //Components
 
 const MainPage = () => {
-
-  const isLargeDesktop = useMediaQuery((theme) => theme.breakpoints.up("xl"));
-  const isDesktop = useMediaQuery((theme) =>theme.breakpoints.between("lg", "xl"));
-  const isNotebook = useMediaQuery((theme) =>theme.breakpoints.between("md", "lg"));
-  const isTablet = useMediaQuery((theme) =>theme.breakpoints.between("sm", "md"));
-  const isMobile = useMediaQuery((theme) => theme.breakpoints.down("sm"));
-
   return (
     <MainPageStyle>
       <UTPSlide>
